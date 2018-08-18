26a03e0916f7f828ca2252efca38f0956b49b057
akocdee <brian@easypos.com.ph>
2018-08-18 04:06:10 -0700

    brian commit

--- template/header.php
+++ template/header.php
@@ -10,6 +10,8 @@
    <link rel='stylesheet' type='text/css' media='screen' href='template/style.css' />
    <!-- cdn -->
    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <!-- font-awesome -->
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
@@ -19,14 +21,24 @@
       <div id= "food_icon">
          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <!-- <a href="https://icons8.com">Icon pack by Icons8</a> -->
-            <img src="https://png.icons8.com/ios/50/000000/dining-room.png" style="width: 32px; height: 32px;">
+            <i class="fas fa-utensils" id = "icon-color1"></i>
          </button>
          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
             <a class="dropdown-item" href="#">Action</a>
             <a class="dropdown-item" href="#">Another action</a>
             <a class="dropdown-item" href="#">Something else here</a>
          </div>
-         <div class="header-divider"></div>
+         <div class="vl"></div>
+         <div class="pageNavHolder">
+                <ul>
+                        <li><a href="#">Test Menu 1</a></li>
+                        <li><a href="#">Test Menu 2</a></li>
+                        <li><a href="#">Test Menu 3</a></li>
+                        <li><a href="#">Test Menu 4</a></li>
+                      </ul>
+         </div>
+          
+         </ul>
       </div>
    </div>
 </body>
